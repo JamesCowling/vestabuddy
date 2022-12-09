1c9832a453c78b15ddee8f7a6ce0a57b22af632f
James Cowling <james@convex.dev>
2022-12-09 14:30:39 -0800

    parse from cmdline

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 # python3 -m pip install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple/ convex
 
+import sys
+
 from convex import ConvexClient
 from pprint import pprint
 
+if len(sys.argv) != 3:
+    print("<cmd> <message> <seconds>")
+    sys.exit(0)
+message = sys.argv[1]
+delay = int(sys.argv[2])
+
 client = ConvexClient("https://silent-mosquito-517.convex.cloud")
-client.action("actions/post", "this message will be gone in 60 seconds", 60)
+client.action("actions/post", message, delay)
