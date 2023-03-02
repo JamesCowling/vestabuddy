e87397590742db4e178b89f2d2ccedf14de18255
James Cowling <james@convex.dev>
2023-03-01 17:30:43 -0800

    can't check auth in a scheduled function

--- convex/actions/reset.ts
+++ convex/actions/reset.ts
@@ -2,7 +2,8 @@ import { action } from "../_generated/server";
 import { setVesta } from "./vesta";
 
 export default action(async ({ runQuery }, text: string) => {
-  await runQuery("checkAuth");
+  // Can't check the auth context here since it's a scheduled function, so this
+  // is actually a security hole.
 
   console.log("resetting vestaboard to %s", text);
   await setVesta(text);
